fc8763b2cd62ca5c190e9a287d6f5815a9b49900
Juan Rivera <juanriv45@gmail.com>
2018-03-06 04:34:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,6 +62,16 @@ class Student
       self.new_from_db(row)
     end
   end
+  def self.count_all_students_in_grade_9
+    # find the student in the database given a name
+    # return a new instance of the Student class
+    sql = <<-SQL
+      SELECT * FROM students WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
+  end
   def self.students_below_12th_grade
     # find the student in the database given a name
     # return a new instance of the Student class
